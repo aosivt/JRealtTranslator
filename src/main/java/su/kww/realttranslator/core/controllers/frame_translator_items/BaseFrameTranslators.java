46afa998b1c4596151b0f54f5ff9c58355751f31
alex <aosivt@gmail.com>
2019-01-09 21:39:47 +0700

    rewrite FrameTranslator.fxml from ancorpane to gridpane

--- src/main/java/su/kww/realttranslator/core/controllers/frame_translator_items/BaseFrameTranslators.java
+++ src/main/java/su/kww/realttranslator/core/controllers/frame_translator_items/BaseFrameTranslators.java
@@ -1,32 +1,24 @@
 package su.kww.realttranslator.core.controllers.frame_translator_items;
 
 import com.jfoenix.controls.JFXTabPane;
-import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import org.apache.commons.codec.binary.Base64;
-import su.kww.realttranslator.core.controllers.item_translator.TranslatorItem;
 
-import java.io.ByteArrayInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public abstract class BaseFrameTranslators implements Initializable {
 
     @FXML
-    protected AnchorPane anchorPane;
-
-    @FXML
-    protected JFXTextField username;
+    protected GridPane anchorPane;
 
     @FXML
     protected ProgressBar progressBar;
